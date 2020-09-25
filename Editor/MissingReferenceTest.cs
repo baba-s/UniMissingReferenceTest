f7284a517ea52c0125400ef0ef41c7c355b5310b
baba_s <shotababa1206@gmail.com>
2020-09-25 21:21:27 +0900

    first commit

---
+++ Editor/MissingReferenceTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Kogane.Internal
+{
+	internal sealed class MissingReferenceTest
+	{
+		[Category( nameof( Kogane ) )]
+		[Test]
+		public void Missing_Reference_が存在しない()
+		{
+			AllGameObjectTester.Test( gameObject => !MissingReferenceUtils.HasMissingReference( gameObject ) );
+		}
+	}
+}
